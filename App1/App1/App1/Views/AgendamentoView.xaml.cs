2b3d0a0d8da84025fbff8bfe035221c20dcc73c0
Huidemar <huidemar@gmail.com>
2017-07-31 22:24:47 -0300

    Finalizado a primeira parte do curso XamarinForms

--- App1/App1/App1/Views/AgendamentoView.xaml.cs
+++ App1/App1/App1/Views/AgendamentoView.xaml.cs
@@ -13,11 +13,34 @@ namespace App1.Views
 	public partial class AgendamentoView : ContentPage
 	{
         public Veiculo Veiculo { get; private set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        DateTime dataAgendamento = DateTime.Today;
+        public DateTime DataAgendamento
+        {
+            get => dataAgendamento;
+            set => dataAgendamento = value;
+        }
+        public TimeSpan HoraAgendamento { get; set; }
+
         public AgendamentoView (Veiculo veiculo)
 		{
 			InitializeComponent ();
             Veiculo = veiculo;
             this.BindingContext = this;
 		}
-	}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Agendamento", 
+string.Format(
+@"Nome: {0}
+Telefone: {1}
+E-mail: {2}
+Data Agendamento: {3}
+Hora Agendamento:{4}",
+Nome,Telefone,Email,DataAgendamento.ToString("dd/MM/yyyy"),HoraAgendamento), "ok");
+        }
+    }
 }
